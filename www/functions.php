3554315d9d034190836d1415caab4003da93b899
nreck <nor@Nicolajs-MacBook-Pro.local>
2017-01-23 14:00:02 +0100

    Integrated WP Nav-walker

--- www/functions.php
+++ www/functions.php
@@ -3,4 +3,12 @@
 //Hides the admin bar in front-end
 add_filter('show_admin_bar', '__return_false');
 
+// Register custom navigation walker
+require_once('modules/navwalker/wp_bootstrap_navwalker.php');
+
+//Register primary menu
+register_nav_menus( array(
+    'primary' => __( 'Header menu', 'www' ),
+) );
+
 ?>
