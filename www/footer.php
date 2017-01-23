f3fbd1fc0f0fdd512ab2ba1ccab3ba554021cd4b
nreck <nor@Nicolajs-MacBook-Pro.local>
2017-01-23 12:58:15 +0100

    Added WP functionality

--- www/footer.php
+++ www/footer.php
@@ -1,3 +1,4 @@
+		<?php wp_footer(); ?>
 		<!-- Placed at the end of the document so the pages load faster -->
 		<script src="../node_modules/jquery/dist/jquery.min.js"></script>
 		<script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
--- www/header.php
+++ www/header.php
@@ -5,14 +5,12 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<link rel="icon" href="../../favicon.ico">
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/favicon.png" />
 
-		<title>Bootstrap starter</title>
+		<title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
 
-		<link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
-		<link rel="stylesheet" href="build/style.min.css">
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
+		<?php wp_head(); ?>
 	</head>
 
 	<body>
