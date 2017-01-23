3d85e0b39b440631dcf53319c01305d474e0957f
nreck <nor@Nicolajs-MacBook-Pro.local>
2017-01-23 13:53:45 +0100

    Changed directories

--- www/footer.php
+++ www/footer.php
@@ -1,7 +1,7 @@
 		<?php wp_footer(); ?>
 		<!-- Placed at the end of the document so the pages load faster -->
-		<script src="../node_modules/jquery/dist/jquery.min.js"></script>
-		<script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-		<script src="build/scripts.min.js"></script>
+		<script src="/wp-content/themes/www/modules/node_modules/jquery/dist/jquery.min.js"></script>
+		<script src="/wp-content/themes/www/modules/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+		<script src="/wp-content/themes/www/build/scripts.min.js"></script>
 	</body>
 </html>
